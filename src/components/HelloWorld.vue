d566e94238e708be48335f14be20db4044cfc329
albbarrera96 <85797748+albbarrera96@users.noreply.github.com>
2022-11-30 18:43:03 -0500

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,31 @@
-<script setup>
+<template>
+    <Nav />
+    <main>
+        <!---HERO GOES HERE---->
+        <div class="container text-light">
+            <p>HELLO</p>
+        </div>
+        <div class="container text-light">
+            
+        </div>
+    </main>
+    
+</template>
+
+<script>
+import Nav from './Navbar.vue';
+export default {
+    components: {
+        Nav
+    }
+}
+
 
 </script>
 
-<template>
-
-</template>
-
 <style scoped>
+    main {
+        background-color: rgb(26, 26, 87);
+    }
 
 </style>
