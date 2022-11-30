5b05ac03f89b1784728d1fc6a28b60d72a0141c6
albbarrera96 <85797748+albbarrera96@users.noreply.github.com>
2022-11-30 18:43:08 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,12 @@
+<template>
+  <HelloWorld />
+  <RouterView />
+</template>
+
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 </script>
-
-<template>
-  <header>
-
-    <div class="wrapper">
-
-    </div>
-  </header>
-
-  <RouterView />
-</template>
 
 <style scoped>
 
